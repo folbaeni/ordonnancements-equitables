b7b44df03ffdab592d93fb8972a9ad4e167b0967
Ali Touzi <ali.touzi@gmail.com>
2020-05-07 18:10:20 +0200

    test avec des class pour les algorithmes

---
+++ OrdonnancementsEquitables/OrdonnancementsEquitables/Algorithmes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdonnancementsEquitables
+{
+    class Algorithmes
+    {
+        protected Algorithmes(){}
+        public virtual void Execute(){}
+        protected String Prefixe() => "Liste des Jobs étudiés:\n";
+        public override String ToString() => "Resultat de l'algorithme:";
+    }
+}
