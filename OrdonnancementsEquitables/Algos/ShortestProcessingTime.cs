5b5013bdeb51686acfd431a794f51e9da0bd3322
Ali Touzi <ali.touzi@gmail.com>
2020-05-15 13:00:02 +0200

    ShortestProcessingTime + Device

---
+++ OrdonnancementsEquitables/Algos/ShortestProcessingTime.cs
@@ -0,0 +1,35 @@
+﻿using OrdonnancementsEquitables.Devices;
+using OrdonnancementsEquitables.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdonnancementsEquitables.Algos
+{
+    class ShortestProcessingTime : Algorithme<Job>, IMultipleDevices<Job>
+    {
+        private Device<Job>[] Devices;
+
+        public override Job[] ExecuteDefault() => null;
+
+        public override Job[] Execute(Job[] jobs) => Execute(jobs, 1);
+
+        public Job[] Execute(Job[] jobs, int nbDevices)
+        {
+            currentJobs = jobs.OrderBy(j => j.Time).ToArray();
+            Devices = new Device<Job>[nbDevices];
+
+            for(int i = 0; i < currentJobs.Length; i++)
+            {
+                Devices[i % nbDevices].AddJob(currentJobs[i]);
+            }
+
+            return Jobs;
+        }
+
+        /*public Job[] Execute(User[] users);
+        public Job[] Execute(User[] users, int nbDevices);*/
+    }
+}
--- OrdonnancementsEquitables/Devices/Device.cs
+++ OrdonnancementsEquitables/Devices/Device.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrdonnancementsEquitables.Jobs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace OrdonnancementsEquitables.Devices
 {
-    public class Device
+    public class Device<TJob> where TJob : Job
     {
+        private List<TJob> _jobs;
+
+        public TJob[] Jobs { get => _jobs.ToArray(); }
+        public int TimeReady { get => _jobs.Sum(j => j.Time);  }
+
+        public Device()
+        {
+            _jobs = new List<TJob>();
+        }
+
+        public void AddJob(TJob job)
+        {
+            _jobs.Add(job);
+        }
     }
 }
