b3f72f74f4a0679f3b48be528c87063541a36a80
Eyal Vayness <evayness@gmail.com>
2020-05-29 10:40:06 +0200

    parser changement

--- OrdonnancementsEquitables/MainWindow.xaml.cs
+++ OrdonnancementsEquitables/MainWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace OrdonnancementsEquitables
     /// </summary>
     public partial class MainWindow : Window
     {
+        Parser fileParser;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,18 +36,17 @@ namespace OrdonnancementsEquitables
         private void OnFileLoaded(string filename)
         {
             filePath.Text = filename;
-            Job[] content = Parser.ParseFromFile(filename, out Type jobType);
+            fileParser = new Parser(filePath.Text);
 
             var assembly = Assembly.GetAssembly(typeof(Algorithme<>)).GetTypes().ToList();
             var children = assembly.Where(t => t.IsClass && t.Namespace == typeof(Algorithme<>).Namespace && t.IsPublic).ToList();
-            var typed = children.Where(t => t.BaseType.IsGenericType && t.BaseType.GetGenericArguments().FirstOrDefault() == jobType).ToList();
+            var typed = children.Where(t => t.BaseType.IsGenericType && t.BaseType.GetGenericArguments().FirstOrDefault() == fileParser.JobType).ToList();
 
             if (typed.Count > 0)
             {
                 SelAlgo.Items.Clear();
                 typed.ForEach(t => SelAlgo.Items.Add(t.Name.SystToAff()));
                 //SelAlgo.SelectedIndex = 0;
-                SelAlgo.Focus();
             }
         }
 
@@ -68,20 +69,21 @@ namespace OrdonnancementsEquitables
 
         private void StartAlgo(object sender, RoutedEventArgs e)
         {
-            Job[] content = Parser.ParseFromFile(filePath.Text, out Type jobType);
             string nomAlgo = SelAlgo.SelectedItem.ToString().AffToSyst();
-            Type algoType = Type.GetType(typeof(Algorithme<Job>).Namespace + "." + nomAlgo);
+            Type algoType = Type.GetType(typeof(Algorithme<>).Namespace + "." + nomAlgo);
             var algo = Activator.CreateInstance(algoType);
 
-            switch (jobType.Name)
+            switch (fileParser.JobType.Name)
             {
                 case "Job":
-                    ((Algorithme<Job>)algo).Execute(content);
-                    Console.WriteLine(((Algorithme<Job>)algo).ToString());
+                    Algorithme<Job> algorithmeJ = (Algorithme<Job>)algo;
+                    algorithmeJ.Execute(fileParser.ParseJobsFromJSON<Job>());
+                    Console.WriteLine(algorithmeJ);
                     break;
                 case "JobP":
-                    ((Algorithme<JobP>)algo).Execute(Parser.ParseFromFile<JobP>(filePath.Text));
-                    Console.WriteLine(((Algorithme<JobP>)algo).ToString());
+                    Algorithme<JobP> algorithmeJP = (Algorithme<JobP>)algo;
+                    algorithmeJP.Execute(fileParser.ParseJobsFromJSON<JobP>());
+                    Console.WriteLine(algorithmeJP);
                     break;
             }
         }
