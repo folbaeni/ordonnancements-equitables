2e39c665a83f6ffc91c775d8d59815cc63b4ab3b
Ali Touzi <ali.touzi@gmail.com>
2020-06-04 18:33:11 +0200

    execute dans HigherLockDegree

--- OrdonnancementsEquitables/Algos/HigherLockDegree.cs
+++ OrdonnancementsEquitables/Algos/HigherLockDegree.cs
@@ -16,33 +16,51 @@ namespace OrdonnancementsEquitables.Algos
             throw new NotImplementedException();
         }
 
+
         public override JobCo[] Execute(JobCo[] jobs)
         {
-            throw new NotImplementedException();
-        }
-
-        /*public override JobCo[] Execute(JobCo[] jobs)
-        {
             GraphLock G = new GraphLock(jobs);
-            JobCo[] trie = new JobCo[];
+            JobCo[] trie = new JobCo[jobs.Length];
             JobCo tmp;
             int parcouru = jobs.Length;
+            int cpt;
             if (parcouru == 1)
-                return jobs[0];
+                return jobs;
 
-            while(parcouru > 0)
+            while(parcouru > 0) 
             {
                 tmp = G.GetHigherDegree();
                 if (tmp.IsLocked)
                 {
-                    JobCo[] depend = new JobCo[];
+
+                    JobCo[] depend = new JobCo[tmp.Depend.Length];
                     foreach(int id in tmp.Depend)
                     {
-                        depend.Append(jobs.Where(j => j.Id = id));
+                        depend.Append((JobCo)Jobs.Where(jobs => jobs.Id == id));
                     }
-                    G.ExecuteJob(tmp);
+                    cpt = 0;
+                    /* foreach(JobCo j in depend) /* risque de boucle infini en cas d'interblocage ( arrive en cas de forte connexité) */
+                   /* {
+                        if (j.Depend.Length != depend.Length)
+                            break;
+                        else
+                        {
+                            if (depend.Contains(j.Id))
+                                cpt++;                            
+                        }
+                    }
+
+                    if(cpt == depend.Length)
+                    {
+                        Console.WriteLine("Erreur: cas d'interblocage");
+                        return null;
+                    } */
                 }
-            }            
+                G.ExecuteJob(tmp);
+                trie.Append(tmp);
+                parcouru--;
+            }
+            return trie;
         }
-    }*/
+    }
     }
